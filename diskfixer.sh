29887b504fd5e9f4050b8db9157e449be96762ea
duc0n <zgamegaming@gmail.com>
2023-07-16 17:34:44 +0700

    Update diskfixer.sh

--- diskfixer.sh
+++ diskfixer.sh
@@ -9,14 +9,14 @@ echo -e "For CentOS version: \033[0;92m7 2009\033[0m"
 echo -e "==================================================="
 echo -e ""
 startTime=$(date +%s)
-
+vg_name=$(vgs --no-headings -o vg_name | awk '{print $1; exit}')
 home_lvs_output=$(lvs --no-headings -o lv_size /dev/$vg_name/home)
 home_lsize=$(echo -e "$home_lvs_output" | awk '{print $1}')
 
 root_lvs_output=$(lvs --no-headings -o lv_size /dev/$vg_name/root)
 root_lsize=$(echo -e "$root_lvs_output" | awk '{print $1}')
 
-vg_name=$(vgs --no-headings -o vg_name | awk '{print $1; exit}')
+
 
 mountS() {
     echo -e "[\033[0;91m@\033[0m] Creating 'temp' folder. And moving 'home' content to it.."
@@ -26,7 +26,7 @@ mountS() {
     echo -e "[\033[0;91m@\033[0m] Unmounting 'home' directory.."
     umount -fl /home
     
-    echo -e "[\033[0;91m@\033[0m] Remove the home LVM volume.."
+    echo -e "[\033[0;91m@\033[0m] Remove the 'home' LVM volume.."
     lvremove /dev/$vg_name/home
     
     fixed_size=${home_lsize^^}
@@ -34,36 +34,38 @@ mountS() {
     lvextend -L+$fixed_size /dev/$vg_name/root
     
     echo -e "[\033[0;91m@\033[0m] Resize the root partition"
-    xfs_growfs /dev/mapper/cl-root
+    xfs_growfs /dev/mapper/$vg_name-root
     
-    echo -e "[\033[0;91m@\033[0m] Copy the /home contents back into the /home directory"
+    echo -e "[\033[0;91m@\033[0m] Copy the 'home' contents back into the 'home' directory"
     cp -a /temp/home /
     
     echo -e "[\033[0;91m@\033[0m] Remove the temporary location"
     yes | rm -rf /temp
     
     
-    echo -e "[\033[0;91m@\033[0m] Remove the entry from /etc/fstab"
+    echo -e "[\033[0;91m@\033[0m] Remove the entry from '/etc/fstab'"
     sudo sed -i "s|^/dev/mapper/$vg_name-home /home|# /dev/mapper/$vg_name-home /home|" /etc/fstab
     
     echo -e "[\033[0;91m@\033[0m] Sync systemd up with the changes.."
     dracut --regenerate-all --force
     
+    new_root_lvs_output=$(lvs --no-headings -o lv_size /dev/$vg_name/root)
+    new_root_lsize=$(echo -e "$new_root_lvs_output" | awk '{print $1}')
     endTime=$(date +%s)
     ((outTime = ($endTime - $startTime) / 60))
     echo ""
-    echo -e "[\033[0;92mOK\033[0m] Script finished. Time consumed:\033[32m $outTime \033[0mMinute!"
+    echo -e "[\033[0;92mOK\033[0m] Script finished. Root now currently have $new_root_lsize. Time consumed:\033[32m $outTime \033[0mMinute!"
 }
 
 continuePro() {
     
-    echo -e "[\033[0;96m@\033[0m] Fetching directory from Volume: $vg_name"
+    echo -e "[\033[0;96m*\033[0m] Fetching directory from Volume: $vg_name"
     
-    echo -e "[\033[0;96m@\033[0m] Current 'root' size: $root_lsize"
+    echo -e "[\033[0;96m*\033[0m] Current 'root' size: $root_lsize"
     
-    echo -e "[\033[0;96m@\033[0m] Current 'home' size: $home_lsize"
+    echo -e "[\033[0;96m*\033[0m] Current 'home' size: $home_lsize"
     
-    read -p "[\033[0;95m@\033[0m] Please confirm above information, and do you want to continue? (y/n): " answer
+    read -p "Please confirm above information, and do you want to continue? (y/n): " answer
     
     answer=${answer,,}
     
